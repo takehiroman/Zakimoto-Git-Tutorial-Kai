0bd24c271bf077a136778742da02a1c5c64af556
津崎 <i1658119@cse.kyoto-su.ac.jp>
2017-12-20 13:53:53 +0900

    データベース名の変更

--- src/main/resources/static/js/console.js
+++ src/main/resources/static/js/console.js
@@ -700,11 +700,6 @@ function onHandle(line, report) {
 		addfile = fileName
 	}
 
-	if (commands[0] === "git" && commands.length > 4) {
-		input = commands.slice(0, 2);
-		input = input.join(" ");
-		console.log(input);
-	}
 
 	var file = ss.indexOf(fileName)
 	if (file >= 0) {
